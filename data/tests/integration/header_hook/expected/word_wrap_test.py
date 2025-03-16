d01c8acce6adcbe105669bf916043811df975016
Richard Parker <richardgarryparker@gmail.com>
2025-03-16 13:01:29 +0000

    Adding script mocks for testing

---
+++ data/tests/integration/header_hook/expected/word_wrap_test.py
@@ -0,0 +1,23 @@
+##################################################################
+# File               : /workspaces/data/tests/integration/header_hook/input/word_wrap_test.py
+# Description        : This is a very long description which
+#                      should definitely be wrapped. There's also
+#                      a second line of text that should be
+#                      wrapped
+# Command-line usage : word_wrap_test.py KWARGS ARGS
+# Maintainer(s)      : richard.parker@lifearc.org
+# Created            : 2025-02-08
+# Last updated       : 2025-02-09
+# Change Log :
+#   2025-02-09       : Here there is only a single line of text
+#                      that should be wrapped, but it's so long
+#                      that it should be wrapped to produce two
+#                      new lines of text. Who would use such
+#                      formatting as this? Only a madman
+#   2025-02-08       : First release.
+##################################################################
+
+#################################
+# Initial section
+#################################
+print("Hello world")
